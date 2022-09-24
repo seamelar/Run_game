9db15eda2d73920a4b5be62b4777a152f6bc585a
seamelar <langemelar@gmail.com>
2022-09-24 20:15:47 +0300

    Add project files.

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1541952.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 103337275, 103337465, 103337466, 103337464 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/103341296/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/103341297/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/103341295/1/ammo.js', 'preload' : true},
+];
---
+++ files/assets/103337362/1/README.txt
@@ -0,0 +1,3 @@
+These scripts hold boilerplate code and require some fixes in order to work properly.
+
+If you're following the workshop, please wait until the part where they are meant to be used.
